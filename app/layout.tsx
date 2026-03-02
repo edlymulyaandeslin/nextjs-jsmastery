1841c34515123cd9a4b199bc599a510d1183e3ba
edlymulyaandeslin <edlymulyaandeslin@gmail.com>
2026-03-03 03:31:05 +0700

    Light and Dark Theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import Navbar from "@/components/navigation/navbar";
+import ThemeProvider from "@/context/ThemeProvider";
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
@@ -29,8 +31,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${inter.className} ${spaceGrotesk.variable} antialiased`}>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${inter.className} ${spaceGrotesk.variable} antialiased`}>
+        <ThemeProvider attribute={"class"} defaultTheme="system" enableSystem disableTransitionOnChange>
+          <Navbar />
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,8 @@
 const Page = () => {
   return (
-    <div>
+    <>
       <p className="h1-bold">Welcome to the Nextjs Crash Course</p>
-      <p className="h1-bold font-space-grotesk">Welcome to the Nextjs Crash Course</p>
-    </div>
+    </>
   );
 };
 
