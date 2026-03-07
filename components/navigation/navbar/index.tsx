6acc8fd88d25332ebc8a11ee7cf7539b872ac706
edlymulyaandeslin <edlymulyaandeslin@gmail.com>
2026-03-08 01:50:32 +0700

    MobileNav and NavLinks

--- components/navigation/navbar/index.tsx
+++ components/navigation/navbar/index.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
+import MobileNavigation from "./MobileNavigation";
 import Theme from "./Theme";
 
 const Navbar = () => {
@@ -16,6 +17,8 @@ const Navbar = () => {
 
       <div className="flex-between gap-5">
         <Theme />
+
+        <MobileNavigation />
       </div>
     </nav>
   );
