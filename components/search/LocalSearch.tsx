c0aa29e4fa62eb801cf582f090023d34d9c16cf5
edlymulyaandeslin <edlymulyaandeslin@gmail.com>
2026-03-13 02:56:13 +0700

    Implement Error Handling

--- components/search/LocalSearch.tsx
+++ components/search/LocalSearch.tsx
@@ -44,7 +44,7 @@ const LocalSearch = ({ route, imgSrc, placeholder, otherClasses }: Props) => {
     }, 300);
 
     return () => clearTimeout(delayDebounceFn);
-  }, [searchQuery, router, route, searchParams, pathname]);
+  }, [searchQuery, route, pathname]);
 
   return (
     <div
