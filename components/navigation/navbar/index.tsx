101bd4fa62b1a755f093e2f367e8452c8d3cea40
edlymulyaandeslin <edlymulyaandeslin@gmail.com>
2026-03-18 03:31:17 +0700

    Implement logout and show user info loggin

--- components/navigation/navbar/index.tsx
+++ components/navigation/navbar/index.tsx
@@ -1,9 +1,12 @@
+import { auth } from "@/auth";
+import UserAvatar from "@/components/UserAvatar";
 import Image from "next/image";
 import Link from "next/link";
 import MobileNavigation from "./MobileNavigation";
 import Theme from "./Theme";
 
-const Navbar = () => {
+const Navbar = async () => {
+  const session = await auth();
   return (
     <nav className="flex-between background-light900_dark200 shadow-light-300 fixed z-50 w-full gap-5 p-6 sm:px-12 dark:shadow-none">
       <Link href={"/"} className="flex items-center gap-1">
@@ -17,6 +20,10 @@ const Navbar = () => {
 
       <div className="flex-between gap-5">
         <Theme />
+
+        {session?.user?.id && (
+          <UserAvatar id={session.user.id} name={session.user.name} imageUrl={session.user.image} />
+        )}
 
         <MobileNavigation />
       </div>
