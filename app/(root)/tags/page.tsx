2f02e3ca3f2b8857f4a060c44ca6a7c5b63390c7
edlymulyaandeslin <edlymulyaandeslin@gmail.com>
2026-03-20 07:08:19 +0700

    Implement getTags server action

--- app/(root)/tags/page.tsx
+++ app/(root)/tags/page.tsx
@@ -1,4 +1,14 @@
-const Tags = () => {
+import { getTags } from "@/lib/actions/tag.action";
+
+const Tags = async () => {
+  const { success, data, errors } = await getTags({
+    page: 1,
+    pageSize: 10,
+    query: "",
+  });
+
+  const { tags } = data || {};
+
   return <div>Tags</div>;
 };
 
