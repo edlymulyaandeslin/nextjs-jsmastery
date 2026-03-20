1ef698e69c0af85548ed7f62edbed8dfa0c65dad
edlymulyaandeslin <edlymulyaandeslin@gmail.com>
2026-03-21 05:59:53 +0700

    Implement getTagQuestions action

--- components/cards/TagCard.tsx
+++ components/cards/TagCard.tsx
@@ -57,7 +57,7 @@ const TagCard = ({ _id, name, questions, showCount, compact, remove, isButton, h
 
   return (
     <Link href={ROUTES.TAG(_id)} className="shadow-light100_darknone">
-      <article className="background-light900_dark200 light-border flex w-full flex-col rounded-2xl border px-6 py-8 sm:w-[250]">
+      <article className="background-light900_dark200 light-border flex w-full flex-col rounded-2xl border px-6 py-8 md:w-[250]">
         <div className="flex items-center justify-between gap-3">
           <div className="background-dark400_light900 w-fit rounded-sm px-5 py-1.5">
             <p className="paragraph-semibold text-dark300_light900">{name}</p>
--- lib/actions/tag.action.ts
+++ lib/actions/tag.action.ts
@@ -1,9 +1,10 @@
-import { Tag } from "@/database";
-import { ActionResponse, ErrorResponse, PaginatedSearchParams } from "@/types/global";
+import { Question as QuestionModel, Tag as TagModel } from "@/database";
+import { GetTagQuestionsParams } from "@/types/action";
+import { ActionResponse, ErrorResponse, PaginatedSearchParams, Question, Tag } from "@/types/global";
 import { QueryFilter } from "mongoose";
 import action from "../handlers/action";
 import { handleError } from "../handlers/error";
-import { PaginatedSearchParamsSchema } from "../validation";
+import { GetTagQuestionsSchema, PaginatedSearchParamsSchema } from "../validation";
 
 export const getTags = async (
   params: PaginatedSearchParams
@@ -21,12 +22,12 @@ export const getTags = async (
   const skip = (Number(page) - 1) * pageSize;
   const limit = pageSize;
 
-  const filterQuery: QueryFilter<typeof Tag> = {};
+  const filterQuery: QueryFilter<typeof TagModel> = {};
 
   if (query) {
     filterQuery.$or = [
       {
-        name: { $regex: new RegExp(query, "i") },
+        name: { $regex: query, $option: "i" },
       },
     ];
   }
@@ -52,13 +53,67 @@ export const getTags = async (
   }
 
   try {
-    const totalTags = await Tag.countDocuments(filterQuery);
+    const totalTags = await TagModel.countDocuments(filterQuery);
 
-    const tags = await Tag.find(filterQuery).sort(sortCriteria).skip(skip).limit(limit);
+    const tags = await TagModel.find(filterQuery).sort(sortCriteria).skip(skip).limit(limit);
 
     const isNext = totalTags > skip + tags.length;
 
     return { success: true, data: { tags: JSON.parse(JSON.stringify(tags)), isNext } };
+  } catch (error) {
+    return handleError(error) as ErrorResponse;
+  }
+};
+
+export const getTagQuestions = async (
+  params: GetTagQuestionsParams
+): Promise<ActionResponse<{ tag: Tag; questions: Question[]; isNext: boolean }>> => {
+  const validationResult = await action({
+    params,
+    schema: GetTagQuestionsSchema,
+  });
+
+  if (validationResult instanceof Error) {
+    return handleError(validationResult) as ErrorResponse;
+  }
+
+  const { tagId, page = 1, pageSize = 10, query } = params;
+  const skip = (Number(page) - 1) * pageSize;
+  const limit = pageSize;
+
+  try {
+    const tag = await TagModel.findById(tagId);
+    if (!tag) throw new Error("Tag not found");
+
+    const filterQuery: QueryFilter<typeof QuestionModel> = {
+      tags: { $in: [tagId] },
+    };
+
+    if (query) {
+      filterQuery.title = { $regex: query, $option: "i" };
+    }
+
+    const totalQuestions = await QuestionModel.countDocuments(filterQuery);
+
+    const questions = await QuestionModel.find(filterQuery)
+      .select("_id title views answers upvotes downvotes author createdAt")
+      .populate([
+        { path: "author", select: "name image" },
+        { path: "tags", select: "name" },
+      ])
+      .skip(skip)
+      .limit(limit);
+
+    const isNext = totalQuestions > skip + questions.length;
+
+    return {
+      success: true,
+      data: {
+        tag: JSON.parse(JSON.stringify(tag)),
+        questions: JSON.parse(JSON.stringify(questions)),
+        isNext,
+      },
+    };
   } catch (error) {
     return handleError(error) as ErrorResponse;
   }
