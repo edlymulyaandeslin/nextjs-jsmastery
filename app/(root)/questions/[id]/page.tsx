3ba2ea60946916b741ea2f29346f8a811fce93c6
edlymulyaandeslin <edlymulyaandeslin@gmail.com>
2026-03-23 02:30:40 +0700

    Implement create answer form

--- app/(root)/questions/[id]/page.tsx
+++ app/(root)/questions/[id]/page.tsx
@@ -1,5 +1,6 @@
 import TagCard from "@/components/cards/TagCard";
 import Preview from "@/components/editor/Preview";
+import AnswerForm from "@/components/forms/AnswerForm";
 import Metrix from "@/components/Metrix";
 import UserAvatar from "@/components/UserAvatar";
 import ROUTES from "@/constants/routes";
@@ -73,6 +74,10 @@ const QuestionDetails = async ({ params }: RouteParams) => {
           <TagCard key={tag._id} _id={tag._id as string} name={tag.name} compact />
         ))}
       </div>
+
+      <section className="my-5">
+        <AnswerForm />
+      </section>
     </>
   );
 };
---
+++ components/forms/AnswerForm.tsx
@@ -0,0 +1,100 @@
+// /components/forms/AnswerForm.tsx
+"use client";
+
+import { standardSchemaResolver } from "@hookform/resolvers/standard-schema";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { Form, FormControl, FormField, FormItem, FormMessage } from "@/components/ui/form";
+import { AnswerSchema } from "@/lib/validation";
+import { MDXEditorMethods } from "@mdxeditor/editor";
+import { Loader } from "lucide-react";
+import dynamic from "next/dynamic";
+import Image from "next/image";
+import { useRef, useState } from "react";
+import { Button } from "../ui/button";
+
+const Editor = dynamic(() => import("@/components/editor"), {
+  // Make sure we turn SSR off
+  ssr: false,
+});
+
+const AnswerForm = () => {
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [isAISubmitting, setIsAISubmitting] = useState(false);
+
+  const editorRef = useRef<MDXEditorMethods>(null);
+
+  const form = useForm<z.infer<typeof AnswerSchema>>({
+    resolver: standardSchemaResolver(AnswerSchema),
+    defaultValues: {
+      content: "",
+    },
+  });
+
+  const handleSubmit = async (values: z.infer<typeof AnswerSchema>) => {
+    console.log(values);
+  };
+
+  return (
+    <div>
+      <div className="flex flex-col justify-between gap-5 sm:flex-row sm:items-center sm:gap-2">
+        <h4 className="paragraph-semibold text-dark400_light800">Write your answer here</h4>
+        <Button
+          className="btn light-border-2 text-primary-500 dark:text-primary-500 gap-1.5 rounded-md border px-4 py-2.5 shadow-none"
+          disabled={isAISubmitting}
+        >
+          {isAISubmitting ? (
+            <>
+              <Loader className="mr-2 size-4 animate-spin" />
+              Generating...
+            </>
+          ) : (
+            <>
+              <Image
+                src={"/icons/starts.svg"}
+                alt="Generate AI Answer"
+                width={12}
+                height={12}
+                className="object-contain"
+              />
+              Generate AI Answer
+            </>
+          )}
+        </Button>
+      </div>
+
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(handleSubmit)} className="mt-6 flex w-full flex-col gap-10">
+          <FormField
+            control={form.control}
+            name="content"
+            render={({ field }) => (
+              <FormItem className="flex w-full flex-col gap-3">
+                <FormControl className="mt-3.5">
+                  <Editor value={field.value} editorRef={editorRef} fieldChange={field.onChange} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <div className="flex justify-end">
+            <Button type="submit" className="primary-gradient w-fit" disabled={isSubmitting}>
+              {isSubmitting ? (
+                <>
+                  <Loader className="mr-2 size-4 animate-spin" />
+                  Posting...
+                </>
+              ) : (
+                "Post Answer"
+              )}
+            </Button>
+          </div>
+        </form>
+      </Form>
+    </div>
+  );
+};
+
+export default AnswerForm;
